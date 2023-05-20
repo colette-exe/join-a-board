09f480ee3157f6106c994a92595420d882c67956
colette-exe <auadoptante@up.edu.ph>
2023-05-20 17:11:14 +0800

    feat: add welcome page, redirect with the correct board code

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import { React, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import logo from '../assets/logo.png'
 import './Page.css';
 
 export default function LandingPage() {
+    let navigate = useNavigate();
+    const [inputValue, setInputValue] = useState("");
+    const boardCode = "BOARD1234";
+
+    const checkInput = () => {
+        if (inputValue === boardCode) {
+            navigate('/join-a-board/home');
+        } else {
+            alert("Incorrect Board Code!");
+        }
+    }
+
     return (
         <div className="body">
-            Welcome!
+            <img id="logo" src={logo} />
+            <input placeholder="ENTER BOARD CODE" value={inputValue} onChange={(e) => setInputValue(e.target.value) }/>
+            <button className="button" onClick={() => checkInput()}>SUBMIT</button>
         </div>
     );
 }
