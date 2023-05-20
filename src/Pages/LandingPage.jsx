60927e95dc2082602b80209f68ad09760b8809c3
colette-exe <auadoptante@up.edu.ph>
2023-05-20 15:39:01 +0800

    add: files

---
+++ src/Pages/LandingPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './Page.css';
+
+export default function LandingPage() {
+    return (
+        <div className="body">
+            Welcome!
+        </div>
+    );
+}
---
+++ src/Pages/Mainpage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './Page.css';
+
+export default function Mainpage() {
+    return (
+        <div className="body">
+            Mainpage
+        </div>
+    );
+}
