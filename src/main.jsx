419fda43289cac25e4f3ea072edde1d3d3d472d7
colette-exe <auadoptante@up.edu.ph>
2023-05-22 09:18:00 +0800

    service worker fix (3/?)

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { register as registerServiceWorker } from './serviceWorkerRegistration';
+import * as serviceWorker from '../sw.js';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
+)
 
-registerServiceWorker();
+serviceWorker.register();
